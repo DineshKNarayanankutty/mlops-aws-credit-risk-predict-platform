dfd07b0a5b0f3f5addce8b585824069b8fc7806c
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-28 19:59:09 +0530

    chore: configure AWS, Kubernetes, and Helm providers with EKS cluster auth for prod

---
+++ terraform/environments/prod/provider.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Owner       = "platform-team"
+    }
+  }
+}
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  token                  = data.aws_eks_cluster_auth.this.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    token                  = data.aws_eks_cluster_auth.this.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  }
+}
