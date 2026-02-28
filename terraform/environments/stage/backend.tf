6ffe687be0d473c454639e98a91b3d059d7731eb
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-02-28 19:46:57 +0530

    Configure S3 backend with encrypted state and native lockfile for Stage environment

---
+++ terraform/environments/stage/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "s3" {
+    bucket       = "mlops-aws-credit-risk-s3"
+    key          = "stage/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
