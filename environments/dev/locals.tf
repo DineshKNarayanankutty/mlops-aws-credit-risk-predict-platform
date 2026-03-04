a37686530fc62b17df1a6ed312055f5ec6177d99
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-03-04 14:40:30 +0530

    Add locals for S3 artifact bucket names and training data URIs using current AWS account ID

---
+++ environments/dev/locals.tf
@@ -0,0 +1,21 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  sagemaker_model_artifacts_bucket_name = "${var.project_name}-${var.environment}-model-artifacts-${data.aws_caller_identity.current.account_id}"
+
+  cicd_artifact_bucket_name = "${var.project_name}-${var.environment}-cicd-artifacts-${data.aws_caller_identity.current.account_id}"
+
+  training_input_s3_uri = "s3://${local.cicd_artifact_bucket_name}/training/input/"
+
+  training_output_s3_uri = "s3://${local.cicd_artifact_bucket_name}/training/output/"
+
+  training_data_bucket_name = local.cicd_artifact_bucket_name
+  training_data_bucket_arn  = "arn:aws:s3:::${local.training_data_bucket_name}"
+
+  common_tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Owner       = "platform-team"
+  }
+}
