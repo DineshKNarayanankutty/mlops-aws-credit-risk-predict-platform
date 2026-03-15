2fc4908bee392f82b1cdd3e1bf9db405e84d33e8
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-03-15 19:26:04 +0530

    feat(vpc): add descriptions and new outputs — vpc_cidr_block, nat_gateway_ids, az_count, vpc_endpoint_s3_id

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,34 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
 }
 
 output "public_subnets" {
-  value = aws_subnet.public[*].id
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnets" {
-  value = aws_subnet.private[*].id
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of NAT Gateways"
+  value       = aws_nat_gateway.nat[*].id
+}
+
+output "az_count" {
+  description = "Number of AZs provisioned"
+  value       = local.az_count
+}
+
+output "vpc_endpoint_s3_id" {
+  description = "ID of the S3 Gateway VPC Endpoint"
+  value       = aws_vpc_endpoint.s3.id
 }
